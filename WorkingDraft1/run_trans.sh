96aea7fa764f85d423b899aed3563cb74972cf7f
Jeffrey Tao <jat2164@columbia.edu>
2015-04-15 17:26:34 -0400

    Build instructions and fixed build script

--- WorkingDraft1/run_trans.sh
+++ WorkingDraft1/run_trans.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 grammar=$1
 test=$2
+alias antlr4='java -jar ./antlr-4.0-complete.jar'
 
 echo "Building Grammar and Running Translater"
 antlr4 $grammar.g4
 javac $grammar*.java
 javac Translate.java
 cat $test | java Translate
+javac Main.java
