982c3acc76234669c0f6f55e7655952a7817c108
Noah Stebbins <nsteb1993@gmail.com>
2015-04-17 16:30:15 -0400

    cleaned up tree of code. "turnt src/" contains up-to-date code. "ANTLR simple example/" is just initial setup stuff. removed the giant reference PDF.

--- whit/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Installing ANTLR"
-export CLASSPATH=".:/usr/local/lib/antlr-4.5-complete.jar:$CLASSPATH"
-alias antlr4='java -:jar /usr/local/lib/antlr-4.5-complete.jar'
-alias grun='java org.antlr.v4.runtime.misc.TestRig'
