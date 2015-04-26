15908f962ea4399e2771166bf8b610eb25b0fd48
Jeffrey Tao <jat2164@columbia.edu>
2015-04-25 21:49:35 -0400

    Directory-based testing framework
    See INSTRUCTIONS for details on how to write/run tests

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,25 +1,40 @@
-ANTLR_LOCATION="antlr-4.0-complete.jar"
-JUNIT_LOCATION="junit-4.12.jar:hamcrest-all-1.3.jar"
-TRANS_LOCATION="../turnt src"
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+TEST_DIR=`pwd`
+LIB_DIR=$TEST_DIR
+ANTLR_LOCATION="$LIB_DIR/antlr-4.5-complete.jar"
+JUNIT_LOCATION="$LIB_DIR/junit-4.12.jar:$LIB_DIR/hamcrest-all-1.3.jar"
+TRANS_LOCATION="$TEST_DIR/../src"
 
 if [ -z $1 ]
 then
     for D in `find . -type d`
     do
-        echo $D
+        if [ $D != "." -a $D != ".." ]
+        then
+            TEST_NAME=`basename $D`
+            echo Running Test $TEST_NAME
+            TT=$TEST_NAME.tt
+            TEST=$TEST_NAME.java
+            pushd $D > /dev/null
+            cat $TT | java -cp "$ANTLR_LOCATION:$TRANS_LOCATION:." Translate
+            javac -cp "$JUNIT_LOCATION:." $TEST
+            java -cp "$JUNIT_LOCATION:." org.junit.runner.JUnitCore $TEST_NAME
+            ../clean_test.sh
+            popd > /dev/null
+        fi
     done
-
-    # Build the turnt program
-    cat test.tt | java -cp "$ANTLR_LOCATION:$TRANS_LOCATION:." Translate
-
-    # Compile Test
-    javac -cp "$JUNIT_LOCATION:." HWTest.java
-
-    #Run Test
-    java -cp "$JUNIT_LOCATION:." org.junit.runner.JUnitCore HWTest
-
-    # Clean up
-    ./clean_test.sh
 else
-    echo $1   
+    D=./$1
+    TEST_NAME=`basename $D`
+    echo Running Test $TEST_NAME
+    TT=$TEST_NAME.tt
+    TEST=$TEST_NAME.java
+    pushd $D > /dev/null
+    cat $TT | java -cp "$ANTLR_LOCATION:$TRANS_LOCATION:." Translate
+    javac -cp "$JUNIT_LOCATION:." $TEST
+    java -cp "$JUNIT_LOCATION:." org.junit.runner.JUnitCore $TEST_NAME
+    ../clean_test.sh
+    popd > /dev/null
 fi
+popd > /dev/null
