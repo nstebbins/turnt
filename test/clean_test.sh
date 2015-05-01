09de3709b0c0c6e873a541286f7d46f94fdb8daa
Jeffrey Tao <jat2164@columbia.edu>
2015-05-01 16:26:43 -0400

    Test directory cleanup

--- test/clean_test.sh
+++ test/clean_test.sh
@@ -1,2 +1,2 @@
 rm -f ./*.class
-rm -f Actions.java Directive.java Engine.java Event.java Main.java State.java mainEvent.java startDirective.java
+rm -f Actions.java Directive.java Engine.java Event.java Main.java State.java *Event.java *Directive.java
